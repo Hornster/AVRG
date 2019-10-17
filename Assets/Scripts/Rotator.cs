370f8f232f9d569099c132b15a04e1045ec9d4e9
Karol Kozuch <karol199714@gmail.com>
2019-10-17 23:07:53 +0200

    Changed project to simple scene

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random = System.Random;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    float rotateAnglePerFrame = 1;
+    System.Random randomGenerator = new System.Random();
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    float GenerateFloat()
+    {
+        return (float) randomGenerator.NextDouble();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+        var axis = new Vector3(GenerateFloat(), GenerateFloat(), GenerateFloat());
+        
+        transform.Rotate(axis, rotateAnglePerFrame);
+    }
+}
